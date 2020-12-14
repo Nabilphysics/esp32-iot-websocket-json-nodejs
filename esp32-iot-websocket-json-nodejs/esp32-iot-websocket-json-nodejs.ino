0862ae0e47309b7881afed708e64237b645cc70b
Nabilphysics <nabilsust@gmail.com>
2020-12-14 08:05:40 +0600

    Update esp32-iot-websocket-json-nodejs.ino

--- esp32-iot-websocket-json-nodejs/esp32-iot-websocket-json-nodejs.ino
+++ esp32-iot-websocket-json-nodejs/esp32-iot-websocket-json-nodejs.ino
@@ -4,7 +4,7 @@
    Main Source(nodejs code + esp32 code): https://esp8266-shop.com/blog/websocket-connection-between-esp8266-and-node-js-server/
    EPS8266 Websocket Library: https://github.com/morrissinger/ESP8266-Websocket
    ESP8266 Websocket to ESP32 Websocket Library Conversion: https://techtutorialsx.com/2017/11/01/esp32-arduino-websocket-client/
-   Rewritten with JSON Intigration: Syed Razwanul Haque(Nabil) , https://www.github.com
+   Rewritten with JSON Intigration: Syed Razwanul Haque(Nabil) , https://www.github.com/Nabilphysics
 .
 */
 /*******************Esp8266_Websocket.ino****************************************/
